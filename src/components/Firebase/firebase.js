bfb0b41939a71404395d1bef262c34f8cc144976
Sieg <siegblink@gmail.com>
2019-09-11 06:05:38 +0800

    Add firebase/auth import for side effects

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app'
+import 'firebase/auth'
 
 // The following is an alternative setup
 
@@ -29,7 +30,7 @@ const config = {
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: '',
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
 }
